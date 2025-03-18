02b9acfaf6107e68decac9f935b5aea0d6c114c6
Ishaan Vashist <ishaanvashista@gmail.com>
2025-03-18 17:56:32 +0530

    Delete bluestock_backend directory

--- bluestock_backend/ipo/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import IPOCompany
-
-@admin.register(IPOCompany)
-class IPOCompanyAdmin(admin.ModelAdmin):
-    list_display = ['name', 'symbol', 'issue_price', 'lot_size', 'open_date', 'close_date']
--- bluestock_backend/ipo/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import IPOCompany
-
-class IPOCompanySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IPOCompany
-        fields = '__all__'
